338fbdaae9fe4cf71e1ec0e93613e0a29a5993c9
seasons-4 <seasons400@gmail.com>
2023-12-12 08:44:25 +0900

    feat: 人口構成の種類選択フォームコンポーネント作成
    - radioボタンを共通コンポーネントとしてLabeledRadio/index.tsxに作成
    - 人口構成の選択フォームコンポーネントとしてSelectableCompositionLabels.tsx作成

---
+++ src/features/prefectureChart/components/SelectableCompositionLabels.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import clsx from 'clsx'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { memo } from 'react'
+
+import { LabeledRadio } from '@/components/LabeledRadio'
+
+import styles from './SelectableCompositionLabels.module.scss'
+import { CHART_LABELS, LABEL_QUERY_KEY } from '../constants'
+import { isCompositionPerYearLabel } from '../filters'
+
+/**
+ * 人口構成のラベルを選択するフォーム
+ */
+export const SelectableCompositionLabels = memo(() => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const pathName = usePathname()
+
+  const queryLabel = searchParams.get(LABEL_QUERY_KEY)
+  const currentLabel = isCompositionPerYearLabel(queryLabel)
+    ? queryLabel
+    : CHART_LABELS[0]
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchParams = new URLSearchParams(searchParams.toString())
+    newSearchParams.set(LABEL_QUERY_KEY, e.target.value)
+    newSearchParams.sort()
+    // NOTE: decodeURI() はrouter.push内で実行してくれるため不要だが、明示的に指定しておく（重複による副作用がないため）
+    router.push(`${pathName}?${decodeURI(newSearchParams.toString())}`)
+  }
+
+  return (
+    <form>
+      <fieldset className={clsx(styles['container'])}>
+        <legend className={clsx(styles['title'])}>
+          人口構成の種類を選択してください
+        </legend>
+        <ul className={clsx(styles['flexible-list'])}>
+          {CHART_LABELS.map((label) => (
+            <li key={label}>
+              <LabeledRadio
+                value={label}
+                name={label}
+                checked={label === currentLabel}
+                onChange={onChange}
+              >
+                {label}
+              </LabeledRadio>
+            </li>
+          ))}
+        </ul>
+      </fieldset>
+    </form>
+  )
+})
+
+SelectableCompositionLabels.displayName = 'SelectableCompositionLabels'
